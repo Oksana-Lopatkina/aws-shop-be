dbee76ffe2f0951d2c18c7553fc9cb451bb9d8a4
Oksana Lopatkina <oksana_lopatkina@epam.com>
2023-03-09 15:09:11 +0300

    implement sqs & sns

--- product-service/src/functions/createProduct/handler.ts
+++ product-service/src/functions/createProduct/handler.ts
@@ -1,6 +1,6 @@
-import { ddbDocClient } from "@libs/ddbDocClient";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
-import { PutCommandInput } from "@aws-sdk/lib-dynamodb/dist-types/commands";
+// DynamoDB
+import { putItem } from '@service/dynamoDBService'
+// libs
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
@@ -9,28 +9,19 @@ import schema from './schema';
 
 const createProduct: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   try {
-    console.log('request createProduct: ', event);
-    const id = uuidv4();
+    console.log('request [createProduct]: ', event);
     const { count, ...product } = event.body;
 
     if (product.article && product.title && product.description && product.image && product.price && count) {
-        const paramsProduct: PutCommandInput = {
-            TableName: process.env.TABLE_NAME_PRODUCTS,
-            Item: { id, ...product },
-        };
-        const putResultProduct = await ddbDocClient.send(new PutCommand(paramsProduct));
-
-        const putResultStock = await ddbDocClient.send(new PutCommand({
-            TableName: process.env.TABLE_NAME_PRODUCTS_STOCK,
-            Item: {
-                productId: id,
-                count,
-            },
-        }));
+        const id = uuidv4();
+        await putItem(process.env.TABLE_NAME_PRODUCTS, { ...product, id });
+        await putItem(process.env.TABLE_NAME_PRODUCTS_STOCK, {
+            productId: id,
+            count,
+        });
 
         return formatJSONResponse({
-            data: { putResultProduct, putResultStock},
-            // event,
+            message: 'A product has been created',
         });
     }
 
